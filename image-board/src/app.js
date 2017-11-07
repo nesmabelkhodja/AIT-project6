f8b40d57d323be2ad1de00cda40501307bad5ef6
nesmabelkhodja <nb2253@nyu.edu>
2017-11-06 23:19:07 -0500

    Part 3

--- image-board/src/app.js
+++ image-board/src/app.js
@@ -1,18 +1,54 @@
+const express = require('express');
+require('./db');
+const bodyParser = require('body-parser');
+const app = express();
 const mongoose = require('mongoose');
 const ImagePost = mongoose.model('ImagePost');
+const Image = mongoose.model('Image');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'))
+
 app.set('view engine', 'hbs');
 
 // body parser setup
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => {
-  res.render('index');
+  res.redirect('/image-posts');
+});
+
+app.get('/image-posts', (req, res) => {
+	ImagePost.find({}, function(err, imageposts, count) {
+    if(err) {
+      res.send(err); 
+    }
+  res.render('home', {posts: imageposts});
+  });
+  // res.render('home');
+});
+
+app.post('/image-posts', function(req, res) {
+		let imgs = []
+
+		for (i=1; i<=3; i++){
+			if (req.body['url'+i] !== ''){
+				imgs.push(new Image({
+					caption: req.body['comment'+i],
+					url: req.body['url'+i]
+				})
+				);
+			}
+		}
+
+		new ImagePost({
+		title: req.body.title,
+		images: imgs
+	}).save(function(err, imageposts, count){
+		if (err)
+			console.log(err);
+		//console.log("posts", posts);
+		res.redirect('/image-posts');
+	});
 });
 
 app.listen(3000);
--- image-board/src/db.js
+++ image-board/src/db.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 const URLSlugs = require('mongoose-url-slugs');
 
 // my schema goes here!
-const Image = new Schema({
+const Image = new mongoose.Schema({
   caption:  String,
   url: String
 });
 
-const ImagePost = new Schema({
+const ImagePost = new mongoose.Schema({
   title:  String,
   images: [Image]
 });
 
-ImagePost.plugin(URLSlugs('title', {field: mySlug}));
+//ImagePost.plugin(URLSlugs('title', {field: mySlug}));
 
 mongoose.model('Image', Image);
 mongoose.model('ImagePost', ImagePost);
